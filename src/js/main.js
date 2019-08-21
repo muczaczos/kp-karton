57321099473339bfd7f82fb061ab01a210f785a2
Arek <muczaczos@gmail.com>
2019-08-21 22:13:46 +0200

    feat: added full page overlay menu for mobiles

--- src/js/main.js
+++ src/js/main.js
@@ -145,3 +145,14 @@ $(window).on("load",function() {
     
   });
 
+
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
